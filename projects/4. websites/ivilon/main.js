6fe611745a8bdc1e38233980a2c71db943ebcc19
James Liu <jamesliu7771@gmail.com>
2021-02-01 01:23:43 -0500

    updated cards with links and text

--- projects/4. websites/ivilon/main.js
+++ projects/4. websites/ivilon/main.js
@@ -1,78 +1,222 @@
 
-
-
-const url4 = 'https://www.amazon.com/dp/B06X9H5HD5?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url5 = 'https://www.amazon.com/dp/B06X9H3B9V?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url6 = 'https://www.amazon.com/dp/B06WWP9XXB?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url7 = 'https://www.amazon.com/dp/B01GER4VGQ?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url8 = 'https://www.amazon.com/dp/B071WQGFT8?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url9 = 'https://www.amazon.com/dp/B01GER4KLM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url10 = 'https://www.amazon.com/dp/B07B2KX563?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url11 = 'https://www.amazon.com/dp/B07B2KDHVD?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url12 = 'https://www.amazon.com/dp/B07B2LHGF4?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url13 = 'https://www.amazon.com/dp/B07B7WRF7M?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url14 = 'https://www.amazon.com/dp/B06XC7BGZ7?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url15 = 'https://www.amazon.com/dp/B074MDB9P7?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url16 = 'https://www.amazon.com/dp/B07K7TPD4J?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url17 = 'https://www.amazon.com/dp/B07K7V66QV?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url18 = 'https://www.amazon.com/dp/B01N6UPROM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url19 = 'https://www.amazon.com/dp/B01GIQ0PVS?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url20 = 'https://www.amazon.com/dp/B01GIQ0PTU?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url21 = 'https://www.amazon.com/dp/B07BD42PQC?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url22 = 'https://www.amazon.com/dp/B01MSB6DN6?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url23 = 'https://www.amazon.com/dp/B07BD3ZSQP?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url24 = 'https://www.amazon.com/dp/B01GOTKHVM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url25 = 'https://www.amazon.com/dp/B07BD2TL74?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url26 = 'https://www.amazon.com/dp/B079K3H3RK?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url27 = 'https://www.amazon.com/dp/B07BD3LMTM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url28 = 'https://www.amazon.com/dp/B01GOTKHSA?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url29 = 'https://www.amazon.com/dp/B07FGBCR45?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-const url30 = 'https://www.amazon.com/dp/B07DTW8XVR?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1';
-
 const data = [
   {
-    link:"https://www.amazon.com/dp/B07GY3FD2W?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
-    image:"css/images/11.jpg",
-    line1:"Ivilon Drapery Treatment",
-    line2:"Window Curtain Rod - Acrylic..",
-    price:"$28.99"
-  },
-  {
     link:"https://www.amazon.com/dp/B07GY18321?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
-    image:"css/images/12.jpg",
+    image:"css/images/2.jpg",
     line1:"Ivilon Drapery Treatment",
     line2:"Window Curtain Rod - Acrylic..",
     price:"$28.99"
   },
   {
     link:"https://www.amazon.com/dp/B07GXYB4ZM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
-    image:"css/images/13.jpg",
+    image:"css/images/3.jpg",
     line1:"Ivilon Drapery Treatment",
     line2:"Window Curtain Rod - Acrylic..",
+    price:"View price on Amazon"
+  },
+  {
+    link:"https://www.amazon.com/dp/B06X9H5HD5?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/4.jpg",
+    line1:"Ivilon Drapery Window Curtain",
+    line2:"Rod - End Cap Style Design 1..",
+    price:"$25.40"
+  },
+  {
+    link:"https://www.amazon.com/dp/B06X9H3B9V?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/5.jpg",
+    line1:"Ivilon Drapery Window Curtain",
+    line2:"Rod - End Cap Style Design 1..",
+    price:"$25.40"
+  },
+  {
+    link:"https://www.amazon.com/dp/B06WWP9XXB?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/6.jpg",
+    line1:"Ivilon Drapery Window Curtain",
+    line2:"Rod - End Cap Style Design 1..",
+    price:"$25.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01GER4VGQ?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/7.jpg",
+    line1:"Ivilon Drapery Window Curtain",
+    line2:"Rod - Twisted Style Set, 28 to ...",
+    price:"View price on Amazon"
+  },
+  {
+    link:"https://www.amazon.com/dp/B071WQGFT8?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/8.jpg",
+    line1:"Ivilon Window Curtain Rod -",
+    line2:"Edge Style Drapery Treatments...",
+    price:"$23.92"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01GER4KLM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/9.jpg",
+    line1:"Ivilon Drapery Window Curtain",
+    line2:"Rod - Twisted Style Set, 28 to ...",
+    price:"View price on Amazon"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07B2KX563?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/10.jpg",
+    line1:"Ivilon Drapery Eyelet Curtain",
+    line2:'Rings - 2.3" Ring for Curtain...',
+    price:"$8.41"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07B2KDHVD?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/11.jpg",
+    line1:"Ivilon Drapery Eyelet Curtain",
+    line2:'Rings - 1.7" Ring for Curtain...',
+    price:"$7.90"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07B2LHGF4?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/12.jpg",
+    line1:"Ivilon Drapery Eyelet Curtain",
+    line2:'Rings - 2" Ring Loop for Hook..',
+    price:"$8.19"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07B7WRF7M?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/13.jpg",
+    line1:"Ivilon Drapery Curtain Clip",
+    line2:"Rings - Clips Ring for Curtain...",
+    price:"$7.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B06XC7BGZ7?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/14.jpg",
+    line1:"Ivilon Drapery Curtain Clips with",
+    line2:"Rings - Set of 14 Decorative...",
+    price:"$8.75"
+  },
+  {
+    link:"https://www.amazon.com/dp/B074MDB9P7?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/15.jpg",
+    line1:"Ivilon Draperies Window Curtain",
+    line2:"Rod - Fleur Design, 28 to 48...",
+    price:"View price on Amazon"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07K7TPD4J?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/16.jpg",
+    line1:"Ivilon Curtain Rod with Ball",
+    line2:"Finials - 1 inch Pole. 28 to 48...",
     price:"$26.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07K7V66QV?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/17.jpg",
+    line1:"Ivilon Curtain Rod with Ball",
+    line2:"Finials - 1 inch Pole. 28 to 48...",
+    price:"$24.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01N6UPROM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/18.jpg",
+    line1:"Ivilon Drapery Treatment",
+    line2:"Window Curtain Rod - Ball...",
+    price:"$22.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01GIQ0PVS?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/19.jpg",
+    line1:"Ivilon Drapery",
+    line2:"Window Curtain Rod - Ball...",
+    price:"$23.35"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01GIQ0PTU?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/20.jpg",
+    line1:"Ivilon Drapery Treatment",
+    line2:"Window Curtain Rod - Ball...",
+    price:"$22.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07BD42PQC?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/21.jpg",
+    line1:"Ivilon Decorative Window",
+    line2:"Curtain Rod - Carved Square...",
+    price:"$24.35"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01MSB6DN6?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/22.jpg",
+    line1:"Ivilon Drapery Window Curtain",
+    line2:"Rod Set - Square Design 1 1/8..",
+    price:"$23.89"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07BD3ZSQP?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/23.jpg",
+    line1:"Ivilon Decorative Window",
+    line2:"Curtain Rod - Carved Square...",
+    price:"View price on Amazon"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01GOTKHVM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/24.jpg",
+    line1:"Ivilon Drapery Treatment",
+    line2:"Window Curtain Rod - Square..",
+    price:"$25.16"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07BD2TL74?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/25.jpg",
+    line1:"Ivilon Decorative Window",
+    line2:"Curtain Rod - Carved Square...",
+    price:"$24.70"
+  },
+  {
+    link:"https://www.amazon.com/dp/B079K3H3RK?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/26.jpg",
+    line1:"Ivilon Window Treatment",
+    line2:"Curtain Rod - Square Finials, 1..",
+    price:"$23.67"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07BD3LMTM?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/27.jpg",
+    line1:"Ivilon Decorative Window",
+    line2:"Curtain Rod - Carved Square...",
+    price:"$23.97"
+  },
+  {
+    link:"https://www.amazon.com/dp/B01GOTKHSA?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/28.jpg",
+    line1:"Ivilon Drapery Treatment",
+    line2:"Window Curtain Rod - Square..",
+    price:"$24.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07FGBCR45?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/29.jpg",
+    line1:"Ivilon Drapery",
+    line2:"Window Curtain Rod - Acrylic..",
+    price:"$28.99"
+  },
+  {
+    link:"https://www.amazon.com/dp/B07DTW8XVR?tag=wixlabs1234-20&linkCode=ogi&th=1&psc=1",
+    image:"css/images/30.jpg",
+    line1:"Ivilon Fixed Brackets for Curtain",
+    line2:"Rods - for 1 or 1 1/8 Inch Rods..",
+    price:"$7.99"
   }
+
 ]
 
-var old = document.querySelector(".loop").innerHTML
-
-console.log(old);
+var newchange = document.querySelector(".loop").innerHTML
 
 data.forEach(function(t){
-  // addition = "<div class="col"><div><div class="card h-100 text-center">
-  //   <a href="#" class="hoverimage caption text-white">
-  //   <img src="css/images/11.jpg" class="card-img-top mt-5 mb-5" alt="..."></a></div>
-  //   <div><p>Ivilon Drapery Treatment <br> Window Curtain Rod - Acrylic..</p>
-  //   <a href="#" class="btn border border-dark">Buy now on Amazon</a></div></div></div>"
-
-  // document.querySelector(".looplink").innerHTML = t.link;
-  // document.querySelector(".change2").innerHTML = t.line1+"<br>"+t.line2;
-  // var x = document.querySelector(".change1").src;
-
-  document.querySelector(".looplink").innerHTML = "<a href = '#' class='looplink hoverimage caption text-white'><img src='./css/images/11.jpg' class='change1 card-img-top mt-5 mb-5' alt='...'></a>";
-  document.querySelector(".change2").innerHTML = t.line1+"<br>"+t.line2;
-
-  document.querySelector(".loop").innerHTML = document.querySelector(".loop").innerHTML+old;
-  console.log(document.querySelector(".loop").innerHTML = old+ document.querySelector(".loop").innerHTML);
+   old = document.querySelector(".loop").innerHTML
+   document.querySelector(".loop").innerHTML = newchange
+   document.querySelector(".looplink").innerHTML = `<a href = "${t.link}" target='_blank' class='looplink hoverimage caption text-white'><img src='./${t.image}' class='change1 card-img-top mt-5 mb-5' alt='...'></a>`;
+   document.querySelector(".change2").innerHTML = t.line1+"<br>"+t.line2;
+   document.querySelector(".change3").innerHTML = t.price;
+   document.querySelector(".loop").innerHTML = old + document.querySelector(".loop").innerHTML;
 });
+
+
 
 
