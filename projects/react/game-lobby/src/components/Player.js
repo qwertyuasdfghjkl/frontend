46517eef29d971eaeefc8e1c19c97e99ce78dc5f
James Liu <jamesliu7771@gmail.com>
2021-02-13 20:54:07 -0500

    added color changing to each player on choice

--- projects/react/game-lobby/src/components/Player.js
+++ projects/react/game-lobby/src/components/Player.js
@@ -6,22 +6,26 @@ const Player = ({ player, colors, changeColor }) => {
 
 
   return (
-    <div className='task'>
-      <h3>{player.text}</h3>
-      <form className='add-form'>
-        <div className='form-control'>
-          <label>Choose Color</label>
-            <select value={color} onChange={(e) => changeColor(player.id ,e.target.value)}>
-              <option defaultValue={player.color}>{player.color}</option>
-              {colors.map((eachColor) => (
-                <option key={eachColor.id2} value={eachColor.color}>{eachColor.color}</option>
-               ))}
-            </select>
+    <div className='task' style={{background: `${player.color}`}}>
+        <h3>{player.text}</h3>
+        <form className='add-form'>
+          <div className='form-control'>
+            <label>Choose Color</label>
+              <select value={color} onChange={(e) => changeColor(player.id ,e.target.value)}>
+                <option defaultValue={player.color}>{player.color}</option>
+                {colors.map((eachColor) => (
+                  <option key={eachColor.id2} value={eachColor.color}>{eachColor.color}</option>
+                ))}
+              </select>
 
-        </div>
-      </form>
+          </div>
+        </form>
     </div>
   )
 }
+
+// const changeColor = {
+//   background: `${player.color}`
+// }
 
 export default Player
